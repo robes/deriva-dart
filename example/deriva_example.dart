4b1b01317d6ec34f3eae507ada923e4a3171308b
Rob Schuler <robes@users.noreply.github.com>
2019-06-19 09:31:26 -0700

    parameterized schema:table names in example

--- example/deriva_example.dart
+++ example/deriva_example.dart
@@ -13,23 +13,27 @@ main(List<String> arguments) async {
   // Establish binding to the server
   var client = ERMrestClient(hostname, catalog_id, credential: credential);
 
+  // Define identifiers for schema:table name
+  String schema = 'isa';
+  String table = 'dataset';
+
   try {
     // Get an entity resource
-    var entities = await client.query('/entity/isa:dataset?limit=3');
+    var entities = await client.query('/entity/${schema}:${table}?limit=3');
     print("Queried entities: ${entities}");
 
-    // Add a new dataset entity (all post/puts are by lists of entities)
+    // Add a new entity (all post/puts are by lists of entities)
     entities = [{'title': 'a new dataset', 'project': 311}]; // these are required fields
-    entities = await client.createEntities('isa', 'dataset', entities, defaults: {'id', 'accession', 'released'});
+    entities = await client.createEntities(schema, table, entities, defaults: {'id', 'accession', 'released'});
     print("Created entities: ${entities}");
 
-    // Update the new dataset entity
+    // Update the new entity
     entities[0]['title'] = 'an updated dataset';
-    entities = await client.updateEntities('isa', 'dataset', entities, correlation: {'RID'}, targets: {'title'});
+    entities = await client.updateEntities(schema, table, entities, correlation: {'RID'}, targets: {'title'});
     print("Updated entities: ${entities}");
 
-    // Delete the new dataset entity
-    await client.delete('/entity/isa:dataset/RID=${entities[0]['RID']}');
+    // Delete the new/updated entity
+    await client.delete('/entity/${schema}:${table}/RID=${entities[0]['RID']}');
     print('Deleted the new/updated entity');
   }
   catch (e) {
